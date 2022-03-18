ccfc5049a7afbdbf1e8be11ce501407b23e2884d
Mohammed Saud <md.saud020@gmail.com>
2022-03-18 16:19:04 +0530

    move base mesh setup to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { startLoop } from './engine/loop';
+import { F32 } from './globals';
 import * as raymarchScene from './raymarch-scene';
-import { WORLD } from './setup';
+import { CTX, WORLD } from './setup';
 
-raymarchScene.init();
+const planeCoords = F32([
+    -1,  1,
+    -1, -1,
+     1, -1,
+     1,  1,
+]);
+
+const { vao_, draw_ } = CTX.createMesh_(
+    [ planeCoords, [0, 1, 2, 0, 2, 3] ],
+    [[0, 2]],
+);
+
+raymarchScene.reset();
 
 startLoop(
     (dt) => {
@@ -10,6 +23,9 @@ startLoop(
         WORLD.fixedStep();
     },
     (dt) => {
+        CTX.clear_();
+        vao_.bind_();
         raymarchScene.render(dt);
+        draw_();
     }
 );
--- src/raymarch-scene.ts
+++ src/raymarch-scene.ts
@@ -1,6 +1,6 @@
 import { Body, Plane, Sphere } from 'cannon-es';
 import { FPSCannonCamera } from './camera';
-import { F32, PI } from './globals';
+import { PI } from './globals';
 import { ASPECT, CTX, WORLD } from './setup';
 
 import frag from './shaders/raymarch.frag';
@@ -9,18 +9,6 @@ import vert from './shaders/raymarch.vert';
 import img from './floor.png';
 
 const shader = CTX.shader_(vert, frag).use_();
-
-const planeCoords = F32([
-    -1,  1,
-    -1, -1,
-     1, -1,
-     1,  1,
-]);
-
-const { vao_, draw_ } = CTX.createMesh_(
-    [ planeCoords, [0, 1, 2, 0, 2, 3] ],
-    [[0, 2]],
-);
 const tex = CTX.texture_();
 
 // cam
@@ -36,7 +24,7 @@ const sPos = sphere.position;
 //sphere.addEventListener('collide', () => sphere.velocity.negate());
 
 
-export const init = () => {
+export const reset = () => {
     shader.uniform_`aspect`.u1f_(ASPECT);
     shader.use_();
     tex.setImage_(img).setUnit_(shader.uniform_`uFloorTex`.loc, 0);
@@ -61,11 +49,8 @@ export const update = (dt: number) => {
 
 //let time = 0;
 export const render = (_dt: number) => {
-    CTX.clear_();
-    vao_.bind_();
     shader.uniform_`uCamPos`.u3f_(pPos.x, pPos.y, pPos.z);
     shader.uniform_`uLookDir`.u3f_(cam.lookDir_.x, cam.lookDir_.y, cam.lookDir_.z);
     //shader.uniform_`uTime`.u1f_(time += 1e-3);
     shader.uniform_`uSpherePos`.u3f_(sPos.x, sPos.y, sPos.z);
-    draw_();
 };
